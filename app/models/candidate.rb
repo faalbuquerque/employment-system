13a15cc31ca91a818f71773207bb7a9494d675eb
Fabiane Albuquerque <bii.nett@gmail.com>
2021-02-17 23:58:38 -0300

    Refactoring collaborators and create candidates on Devise

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,8 +1,6 @@
-class Collaborator < ApplicationRecord
+class Candidate < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  belongs_to :company
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   root 'home#index'
 
-  devise_for :collaborators, controllers: { registrations: 'collaborators/registrations' }
+  devise_for :collaborators, path: 'collaborators', controllers: { registrations: 'collaborators/registrations' }
+
+  devise_for :candidates, path: 'candidates'
+
+  #devise_for :collaborators, controllers: { registrations: 'collaborators/registrations' }
 end
