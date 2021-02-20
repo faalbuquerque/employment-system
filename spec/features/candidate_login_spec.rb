6ad219efaabaf21dab1a2c7cab47669c24e9ef04
Fabiane Albuquerque <bii.nett@gmail.com>
2021-02-20 03:29:45 -0300

    Candidate login and Register

---
+++ spec/features/candidate_login_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Candidate Login' do
+  scenario 'successfully' do
+    Candidate.create!(email: 'candidate@test.com', password: 'password')
+
+    visit root_path
+    click_on 'Candidato'
+
+    fill_in 'Email', with: 'candidate@test.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+
+    expect(page).to  have_content 'Login efetuado com sucesso!'
+    expect(page).to  have_content 'Logout'
+  end
+
+  scenario 'blank fields' do
+    visit root_path
+    click_on 'Candidato'
+
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    click_on 'Log in'
+
+    expect(page).to  have_content 'Email ou senha inválida.'
+  end
+end
---
+++ spec/features/candidate_register_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Candidate Register' do
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Candidato'
+    click_on 'Sign up'
+
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+
+    expect(page).to  have_content 'Login efetuado com sucesso.'
+    expect(page).to  have_content 'Logout'
+  end
+
+  scenario 'blank fields' do
+    visit root_path
+    click_on 'Candidato'
+    click_on 'Sign up'
+
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_on 'Sign up'
+
+    expect(page).to  have_content 'Email não pode ficar em branco'
+    expect(page).to  have_content 'Password não pode ficar em branco'
+  end
+end
