1f6524ee89a15c96ab58099dfb1cd3b6e1c308cb
Fabiane Albuquerque <bii.nett@gmail.com>
2021-02-28 00:18:44 -0300

    Update system name Change name to Git+Employ...Gitemploy

--- spec/features/visitor_visit_homepage_spec.rb
+++ spec/features/visitor_visit_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Visitor visit homepage' do
   scenario 'successfully' do
     visit root_path
-    expect(page).to have_content I18n.t('home.index.employment_system')
+    expect(page).to have_content I18n.t(:employment_system_title)
   end
 end
